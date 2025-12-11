dca0408d043b4450790b73715a982ea0c892bb73
digimukul-dev <digi.mukul30@gmail.com>
2025-12-11 16:21:03 +0530

    themeprovider and globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
+import { ThemeProvider , createGlobalStyle} from 'styled-components'
 import react from './assets/react.svg'
 import StyledButton, { FancyButton , SubmitButton } from './Components/Buttons/Button'
 import './App.css'
-
+import { AnimatedLogo, DarkButton } from './Components/Buttons/Buttons.style'
 function App() {
   // const [count, setCount] = useState(0)
-
+const theme = {
+  dark: {
+    primary:'#000',
+    text:'#fff',
+  },
+  light: {
+    primary:'#ffffff',
+    text:'#000',
+  },
+  fontFamily:'Segoe UI',
+}
+const GlobalStyles = createGlobalStyle`
+  button{
+    font-family: ${(props) => props.theme.fontFamily};
+  }
+`
   return (
-    <>
-    <img src={react} className='logo' alt='logo'/>
+    <ThemeProvider theme={theme}>  
+   <>
+   <GlobalStyles />
+    <AnimatedLogo src={react} />
+    {/* <img src={react} className='logo' alt='logo'/> */}
     <br/>
       <StyledButton type="submit">Styled Component</StyledButton>
       <div>
@@ -20,8 +39,10 @@ function App() {
       </div>
         <FancyButton>Fancy Button</FancyButton>
         <div><br/></div>
-        <SubmitButton>Submit</SubmitButton>
+        <SubmitButton>Submit</SubmitButton><br/>
+        <DarkButton>Dark Button</DarkButton>
     </>
+    </ThemeProvider>
   )
 }
 
--- src/Components/Buttons/Buttons.style.jsx
+++ src/Components/Buttons/Buttons.style.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled , {keyframes} from 'styled-components';
 
 export const StyledButton = styled.button`
   border: 2px solid #C6A874;
@@ -50,3 +50,22 @@ export const SubmitButton = styled(StyledButton).attrs({ type: 'submit' })`
     transform: translateY(5px);
   }
 `;
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
+export const AnimatedLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  animation: ${rotate} infinite 2s linear;
+`;
+
+export const DarkButton = styled(StyledButton)`
+ border:2px solid ${props => props.theme.dark.primary};
+ background-color:${(props) => props.theme.dark.primary};
+ color:${(props) => props.theme.dark.text};
+ `;
